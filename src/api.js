01149c6f87e1c43daece0ede3e4243e4d196dc0b
Mishelle23 <michaela.med@outlook.com>
2022-03-15 21:28:34 +0000

    4.3 update

--- src/api.js
+++ src/api.js
@@ -31,7 +31,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'YOUR_GET_EVENTS_API_ENDPOINT' + '/' + token;
+    const url = 'https://91e9gy9mhd.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
