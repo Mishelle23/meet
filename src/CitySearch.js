8670b83e2e1b42a7b4013f615306b50b471c1755
Mishelle23 <michaela.med@outlook.com>
2022-04-02 21:53:38 +0100

    4.7.1

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
+import { InfoAlert } from '/.Alert';
 
 class CitySearch extends Component {
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: undefined,
+    infoText: null
   }
+  // handleInputChanged = (event) => {
+  // const value = event.target.value;
+  //  const suggestions = this.props.locations.filter((location) => {
+  //    return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+  //  });
+  //  this.setState({
+  //    query: value,
+  //    suggestions,
+  //  });
+  //};
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   };
+
+
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
@@ -43,6 +66,7 @@ class CitySearch extends Component {
           ))}
           <li onClick={() => this.handleItemClicked("all")}>
             <b>See all cities</b>
+            <InfoAlert text={this.state.infoText} />
           </li>
         </ul>
       </div>
